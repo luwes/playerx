8908a2c6a687aeb0304a8889e69197947eca4786
Wesley Luyten <me@wesleyluyten.com>
2020-07-07 20:36:54 -0400

    Set up Browserstack webdriverio for benchmark

--- packages/bench/index.js
+++ packages/bench/index.js
@@ -1,8 +1,6 @@
-const playwright = require('playwright');
+const assert = require('assert');
 const minimist = require('minimist');
-const NETWORK_PRESETS = require('./network.js');
 
-const ci = String(process.env.CI).match(/^(1|true)$/gi);
 const argv = minimist(process.argv.slice(2), {
   default: {
     player: null,
@@ -24,98 +22,43 @@ const players = {
   youtube: {},
 };
 
-const randomKey = function (obj) {
+const randomKey = function(obj) {
   var keys = Object.keys(obj);
   return keys[(keys.length * Math.random()) << 0];
 };
 
 const player = argv.player || randomKey(players);
 
-const newyork = {
-  latitude: 40.730610,
-  longitude: -73.935242
-};
+describe('Basic playback', function() {
+  it('plays back the test video', function () {
+    let url = `https://dev.playerx.io/demo/${player}/`;
 
-const saopaulo = {
-  latitude: -23.5475,
-  longitude: -46.6361,
-  network: NETWORK_PRESETS.fast3g
-};
+    browser.url(url);
+    expect(browser).toHaveTitle('Playerx - API Demo');
 
-async function runBenchmark(geolocation = newyork) {
-  const browser = await playwright.chromium.launch({
-    executablePath: ci
-      ? '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-      : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    headless: false,
-    // Network throttling only works if devtools is open
-    devtools: !!geolocation.network,
-    args: ['--disable-web-security', '--no-user-gesture-required'],
+    browser.setTimeout({ script: 30000 });
+
+    console.warn(`Starting playback for ${player}`);
+    assert(browser.executeAsync(async function(done) {
+      const plx = document.querySelector('player-x');
+      await plx.play();
+      done(true);
+    }));
+
+    console.warn(`Seeking 10s from the end for ${player}`);
+    assert(browser.executeAsync(async function(done) {
+      setTimeout(() => {
+        const plx = document.querySelector('player-x');
+        plx.currentTime = plx.duration - 10;
+        done(true);
+      }, 10000);
+    }));
+
+    console.warn(`Waiting until ended for ${player}`);
+    assert(browser.executeAsync(async function(done) {
+      const plx = document.querySelector('player-x');
+      plx.on('ended', () => done(true));
+    }));
+
   });
-  const context = await browser.newContext({
-    geolocation,
-    permissions: ['geolocation']
-  });
-  const page = await context.newPage();
-
-  if (geolocation.network) {
-    // Connect to Chrome DevTools
-    const client = await page.context().newCDPSession(page);
-    // Set throttling property
-    await client.send('Network.emulateNetworkConditions', geolocation.network);
-  }
-
-  // Dailymotion won't start via the player.play() method.
-  // Enable autoplay for it.
-  const autoplay = ['dailymotion'].includes(player);
-
-  let url = `https://dev.playerx.io/demo/${player}/`;
-  if (autoplay) {
-    url += '?autoplay=1';
-  }
-
-  console.warn(`Loading ${url}`);
-  await page.goto(url, {
-    waitUntil: 'domcontentloaded',
-  });
-
-  const plxElementHandle = await page.$('player-x');
-
-  console.warn(`Starting playback for ${player}`);
-  if (!autoplay) {
-    await page.evaluate((plx) => plx.ready(), plxElementHandle);
-    await page.evaluate((plx) => plx.play(), plxElementHandle);
-  }
-
-  await delay(10000);
-  console.warn(`Seeking 10s from the end for ${player}`);
-  await page.evaluate((plx) => {
-    plx.currentTime = plx.duration - 10;
-  }, plxElementHandle);
-
-  console.warn(`Waiting until ended for ${player}`);
-  await Promise.race([
-    delay(20000),
-    page.evaluate(
-      (plx) =>
-        new Promise((resolve) => {
-          plx.on('ended', resolve);
-        }),
-      plxElementHandle
-    ),
-  ]);
-
-  await browser.close();
-}
-
-runBenchmark();
-runBenchmark(saopaulo);
-
-/**
- * Returns a promise that will resolve after passed ms.
- * @param  {number} ms
- * @return {Promise}
- */
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+});
---
+++ packages/bench/parallel.conf.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+const minimist = require('minimist');
+
+const argv = minimist(process.argv.slice(2), {
+  default: {
+    player: null,
+  },
+});
+
+const options = {
+  sessionName: 'Player: ' + argv.player,
+  buildName: 'Playback ' + (process.env.TRAVIS_BUILD_NUMBER || 0),
+  projectName: 'Playerx Benchmark',
+  debug: true,
+  consoleLogs: 'verbose',
+};
+
+const commonCapabilities = {
+  browser: 'chrome',
+  'browserstack.use_w3c': true,
+  'bstack:options': {
+    ...options,
+  },
+};
+
+exports.config = {
+  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACC_KEY',
+
+  updateJob: false,
+  specs: ['./index.js'],
+  exclude: [],
+
+  maxInstances: 10,
+
+  capabilities: [
+    {
+      ...commonCapabilities,
+    },
+    {
+      ...commonCapabilities,
+      'bstack:options': {
+        ...options,
+        osVersion: '8.0',
+        deviceName: 'Samsung Galaxy S9',
+        realMobile: 'true',
+        geoLocation: 'BR',
+        networkProfile: '3.5g-hspa-good',
+      },
+    },
+  ],
+
+  logLevel: 'warn',
+  coloredLogs: true,
+  screenshotPath: './errorShots/',
+  baseUrl: '',
+  waitforTimeout: 1.5 * 60 * 1000, // 1.5min
+  connectionRetryTimeout: 1.5 * 60 * 1000, // 1.5min
+  connectionRetryCount: 3,
+  host: 'hub.browserstack.com',
+
+  before: function () {},
+  framework: 'mocha',
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 2 * 60 * 1000, // 2min
+  },
+};
