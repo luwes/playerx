72e116a53e23142c9684516929a75aafdaaee455
Wesley Luyten <me@wesleyluyten.com>
2020-08-22 21:55:03 -0400

    Disable file dash on Safari

--- packages/playerx/test/players/file-dash.js
+++ packages/playerx/test/players/file-dash.js
@@ -1,10 +1,16 @@
-import { testPlayer } from '../player.js';
+import { testPlayer, defaultBrowsers } from '../player.js';
 
 const src = 'https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd';
 const duration = 194;
 
 const tests = {
-  play: true,
+  play: {
+    browsers: {
+      ...defaultBrowsers(true),
+      // DASH fails on Safari, not sure why
+      safari: false,
+    },
+  },
 };
 
 testPlayer({ src, duration, tests });
