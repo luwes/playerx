3143ed02bbaa957cbc923414711ca33df7a5311c
Wesley Luyten <me@wesleyluyten.com>
2021-04-20 19:53:58 -0700

    Use tailwind css 2xl screen size

--- packages/site/tailwind.config.js
+++ packages/site/tailwind.config.js
@@ -30,12 +30,6 @@ module.exports = {
     cursor: [...variants.cursor, 'disabled'],
   },
   theme: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...fontFamily.sans],
