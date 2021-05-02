85744460c8c8a465301096f4a25540748434a971
Wesley Luyten <me@wesleyluyten.com>
2021-05-02 12:24:17 -0700

    Fix plx-script add to dom player after

--- packages/playerx/src/script.js
+++ packages/playerx/src/script.js
@@ -5,25 +5,41 @@ const props = {
   reflect: {
     loading: undefined,
     src: undefined,
-  }
+  },
 };
 
 const setup = () => (el) => {
-  const player = el.player;
+  let didConnect;
+  connected();
 
-  customElements.whenDefined(player.localName).then(() => {
-    if (el.loading === 'player') {
+  function connected() {
+    const { player } = el;
+    if (!player || didConnect) return;
+
+    didConnect = true;
+
+    if (customElements.get(player.localName)) {
+      onDefined();
+    } else {
+      customElements.whenDefined(player.localName).then(onDefined);
+    }
+
+    function onDefined() {
       const { load } = player;
+
+      // el.loading == player
       Object.assign(player, {
         async load() {
           await loadScript(el.src);
           return load();
         },
       });
-    } else {
-      loadScript();
     }
-  });
+  }
+
+  return {
+    connected,
+  };
 };
 
 export const PlxScript = define('plx-script', {
