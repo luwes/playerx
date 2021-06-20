aaedfcdf2d3471f1f6d3b0b37f475f7d1cba0c4f
Wesley Luyten <me@wesleyluyten.com>
2021-06-20 10:03:07 -0500

    Disable Firefox play test for Vidyard Showed a failing error with chunk downloads

--- packages/playerx/test/players/vidyard.js
+++ packages/playerx/test/players/vidyard.js
@@ -1,9 +1,16 @@
-import { testPlayer } from '../player.js';
+import { testPlayer, defaultBrowsers } from '../player.js';
 
 const src = 'https://share.vidyard.com/watch/TYY9iSji3mJuFqp2oj4FoL?';
 const duration = 46;
 
 const tests = {
+  play: {
+    browsers: {
+      ...defaultBrowsers(true),
+      // DASH fails on Safari, not sure why
+      firefox: false,
+    },
+  },
   volume: {
     async: true,
   },
