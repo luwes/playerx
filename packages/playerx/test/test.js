33ad2d7491ac8e37a62a5e5b3f780d5c824680a2
Wesley Luyten <me@wesleyluyten.com>
2021-06-20 11:06:11 -0500

    Upgrade Karma

--- packages/playerx/test/test.js
+++ packages/playerx/test/test.js
@@ -7,20 +7,20 @@ import './utils.js';
 import '../src/all.js';
 
 // Run the buggy players first
-import './players/facebook.js';
+import './players/html.js';
+import './players/vidyard.js';
+// import './players/theoplayer.js'; // fails on http:// security error
 import './players/dailymotion.js';
+import './players/facebook.js';
 import './players/youtube.js';
 import './players/wistia.js';
 // import './players/twitch.js'; // too buggy, timeouts on play test
 import './players/streamable.js';
 import './players/soundcloud.js';
 import './players/jwplayer.js';
-import './players/vidyard.js';
 import './players/dashjs.js';
 import './players/hlsjs.js';
-import './players/html.js';
 import './players/brightcove.js';
 import './players/vimeo.js';
 import './players/videojs.js';
 import './players/shakaplayer.js';
-import './players/theoplayer.js';
