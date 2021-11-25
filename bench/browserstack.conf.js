888cc2f9b9b05505bad63a6eb1337f79ac405bc0
Wesley Luyten <me@wesleyluyten.com>
2021-11-25 12:17:53 -0600

    Add cloudflare to bench

--- bench/browserstack.conf.js
+++ bench/browserstack.conf.js
@@ -43,6 +43,7 @@ exports.config = {
 
   updateJob: false,
   specs: [
+    'players/cloudflare.js',
     'players/muxvideo.js',
     'players/brightcove.js',
     'players/dailymotion.js',
--- bench/saucelabs.conf.js
+++ bench/saucelabs.conf.js
@@ -39,6 +39,7 @@ exports.config = {
 
   updateJob: false,
   specs: [
+    'players/cloudflare.js',
     'players/muxvideo.js',
     'players/brightcove.js',
     'players/dailymotion.js',
