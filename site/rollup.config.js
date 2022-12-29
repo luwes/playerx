b610367b093295c26299a673d809317991328351
Wesley Luyten <wesley@mux.com>
2022-12-29 15:12:27 -0600

    fix: upgrade deps

--- site/rollup.config.js
+++ site/rollup.config.js
@@ -1,6 +1,5 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { babel } from '@rollup/plugin-babel';
+import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import bundleSize from 'rollup-plugin-size';
 import sourcemaps from 'rollup-plugin-sourcemaps';
@@ -30,11 +29,9 @@ const config = {
   plugins: [
     bundleSize(),
     sourcemaps(),
-    commonjs(),
     nodeResolve(),
 
     babel({
-      babelHelpers: 'bundled',
       inputSourceMap: false,
       compact: false,
     }),
