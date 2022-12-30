d02edeb7007be3c1941031fc6591166278acef6c
Wesley Luyten <wesley@mux.com>
2022-12-30 09:03:39 -0600

    docs: remove autoprefixer

--- site/postcss.config.js
+++ site/postcss.config.js
@@ -2,10 +2,6 @@ const plugins = [
   require('postcss-import'),
   require('tailwindcss/nesting'),
   require('tailwindcss'),
-  require('autoprefixer'),
-  // require('postcss-comment'),
-  // require('postcss-mixins'),
-  // require('postcss-color-mix'),
 ];
 
 if (process.env.NODE_ENV === 'prod') {
