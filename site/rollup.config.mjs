357397580297a79f3968cbaac99bc5747728956c
Wesley Luyten <wesley@mux.com>
2022-12-30 16:16:33 -0600

    chore: upgrade site deps

--- site/rollup.config.mjs
+++ site/rollup.config.mjs
@@ -1,6 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import bundleSize from 'rollup-plugin-size';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
